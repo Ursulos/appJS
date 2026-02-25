6fcfc8d9d993a26a0c63306dbf2378cbbbbd6406
Ursulos <fredjursulos@gmail.com>
2026-02-25 15:14:29 +0300

    Initiation au javascript

---
+++ public/script/script.js
@@ -0,0 +1,17 @@
+//let nom ="Admin"
+//const x = 17
+//const y = 5
+let x = Number(prompt("Saisir un nombre :"))
+let y = Number(prompt ("Saisir un deuxième nombre :"))
+
+const somme = x+y
+const soust = x-y
+const produit = x*y
+const division = x/y
+const modulo = x%y
+
+console.log(x + " + " +y+ " = " +somme)
+console.log(x + " - " +y+ " = " +soust)
+console.log(x + " * " +y+ " = " +produit)
+console.log(x + " / " +y+ " = " +division)
+console.log(x + " % " +y+ " = " +modulo)
