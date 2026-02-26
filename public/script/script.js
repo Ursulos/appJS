cc2de72abf32b78dac45a3599a43ee08e3167dce
Ursulos <fredjursulos@gmail.com>
2026-02-26 22:02:17 +0300

    exercice js

--- public/script/script.js
+++ public/script/script.js
@@ -1,17 +1,34 @@
 //let nom ="Admin"
 //const x = 17
 //const y = 5
-let x = Number(prompt("Saisir un nombre :"))
-let y = Number(prompt ("Saisir un deuxième nombre :"))
+//let x = Number(prompt("Saisir un nombre :"))
+//let y = Number(prompt ("Saisir un deuxième nombre :"))
 
-const somme = x+y
-const soust = x-y
-const produit = x*y
-const division = x/y
-const modulo = x%y
+//const somme = x+y
+//const soust = x-y
+//const produit = x*y
+//const division = x/y
+//const modulo = x%y
 
-console.log(x + " + " +y+ " = " +somme)
-console.log(x + " - " +y+ " = " +soust)
-console.log(x + " * " +y+ " = " +produit)
-console.log(x + " / " +y+ " = " +division)
-console.log(x + " % " +y+ " = " +modulo)
+//console.log(x + " + " +y+ " = " +somme)
+//console.log(x + " - " +y+ " = " +soust)
+//console.log(x + " * " +y+ " = " +produit)
+//console.log(x + " / " +y+ " = " +division)
+//console.log(x + " % " +y+ " = " +modulo)
+let nbrm = 27;
+let nbr;
+for(let i=1; ; i++){
+    nbr = Number(prompt("Essaie " + i + " : entre un nombre entre 10 et 50"));
+    if (nbr<10 || nbr > 50) {
+        alert("le nombre doit être entre 10 et 50 !");
+        continue;
+    }
+    if (nbr<nbrm) {
+        alert("Trop petit ! choisis un nombre plus grand.");
+    } else if (nbr> nbrm) {
+        alert("Trop grand ! choisis un nombre plus petit.") ;
+    } else {
+        alert("Bravo ! tu as trouvé en " + i + " Tu es un génie");
+        break;
+    }
+}
